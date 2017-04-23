c840c5235d88253b8b152f60a54e5ebfc1e34932
Metehan Dagsuyu <metedagsuyu@hotmail.com>
2017-04-23 01:09:03 -0600

    redesign
    cleaner look

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// projects = document.getElementsByClassName("info");
+
+// for(var a = 0; a<projects.length; a++){
+// 	projects[a].addEventListener("click", function(){
+// 		if (document.getElementsByClassName("info")[a].nextSibling.nextSibling.style.display == "block") {
+// 		document.getElementsByClassName("info")[a].nextSibling.nextSibling.style.display = "none";
+// 	} else {
+// 		document.getElementsByClassName("info")[a].nextSibling.nextSibling.style.display = "block";
+// 	}
+// 	})
+// }
+
+
+
+
+
+
+
+ 
