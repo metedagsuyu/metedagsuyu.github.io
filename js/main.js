2d478aea002cde7640ba2135d6d9f4d93d6cb53f
Metehan Dagsuyu <metedagsuyu@hotmail.com>
2017-07-23 13:44:33 -0600

    project info
    pop up toggle project info slide

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-// projects = document.getElementsByClassName("info");
+projects = document.getElementsByClassName("info");
 
-// for(var a = 0; a<projects.length; a++){
-// 	projects[a].addEventListener("click", function(){
-// 		if (document.getElementsByClassName("info")[a].nextSibling.nextSibling.style.display == "block") {
-// 		document.getElementsByClassName("info")[a].nextSibling.nextSibling.style.display = "none";
-// 	} else {
-// 		document.getElementsByClassName("info")[a].nextSibling.nextSibling.style.display = "block";
-// 	}
-// 	})
-// }
+for(var a = 0; a<projects.length; a++){
+	projects[a].addEventListener("click", function(){
+		//change this to close img
+		//toggle the ul 
+		if (this.nextSibling.nextSibling.nextSibling.nextSibling.style.display == "block") {
+			this.src="./img/info.png";
+			this.nextSibling.nextSibling.nextSibling.nextSibling.style.display = "none";
+		} else {
+			this.src = "./img/close.png";
+			this.nextSibling.nextSibling.nextSibling.nextSibling.style.display = "block";
+		}
+	})
+}
 
 
 
